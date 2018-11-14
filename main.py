f520cd528a98dda63fcbcd54e8f3e098a28a9214
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2018-11-14 19:21:37 +0100

    Showing created graph on screen and saving as .png

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from graph_generator import *
+import networkx as nx
+import matplotlib.pyplot as plt
 	
 if __name__ == "__main__":
 	
-	graph = generateGraph()
+	graph = generateGraph(10, 0.5)
 	print(graph)
+	
+	g = nx.Graph()
+	
+	g.add_nodes_from(list(map(lambda x: str(x.id), graph.nodes)))
+	g.add_edges_from(list(map(lambda x: (str(x.node1.id), str(x.node2.id)), graph.edges)))
+	
+	nx.draw(g, with_labels = True)
+	plt.savefig("tmp.png")
+	plt.show()
+	
 	
