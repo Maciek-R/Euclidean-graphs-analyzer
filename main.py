09009b1b5ce9a1414e2bd2a7c4bfda8c2990a712
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2018-11-17 14:23:00 +0100

    Added class for painting graphs.

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from graph_generator import *
 import networkx as nx
 import matplotlib.pyplot as plt
+from graph_painter import *
 	
 if __name__ == "__main__":
 	
 	graph = generateGraph(10, 0.5)
 	print(graph)
 	
-	g = nx.Graph()
-	
-	g.add_nodes_from(list(map(lambda x: str(x.id), graph.nodes)))
-	g.add_edges_from(list(map(lambda x: (str(x.node1.id), str(x.node2.id)), graph.edges)))
-	
-	nx.draw(g, with_labels = True)
-	plt.savefig("tmp.png")
-	plt.show()
+	g = preparePainterGraph(graph)
+	drawPainterGraph(g)
 	
 	
