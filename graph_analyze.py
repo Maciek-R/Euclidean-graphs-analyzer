ecfd54d2fad51056cf92583d93de6b1e9987d0b5
akowalew <ram.techen@gmail.com>
2019-01-09 10:15:43 +0100

    graph_analyze: fixed wrong access to dictionary read from max_comps_sizes.json file

--- graph_analyze.py
+++ graph_analyze.py
@@ -8,65 +8,80 @@ from multiprocessing import Pool
 from common import *
 from graph_gen import GraphGenerator
 
-ComponentsSizesSet = Dict[int, Dict[float, List[int]]]
+ComponentsSizesSet = Dict[str, Dict[str, List[int]]]
 
 
 class GraphAnalyzer:
     output_dir: str
     generator: GraphGenerator
-    log: logging.Logger
 
     def __init__(self, output_dir: str, generator: GraphGenerator) -> None:
         self.output_dir = output_dir
         self.generator = generator
-        self.log = logging.getLogger("GraphAnalyzer")
+
+    @property
+    def logger(self):
+        return logging.getLogger("GraphAnalyzer")
 
     def __call__(self, sizes: Sequence[int],
                  radiuses: Sequence[float],
                  count: int,
                  pool: Pool) -> ComponentsSizesSet:
         assert count >= 0
-        self.log.debug("Performing analysis...")
 
+        self.logger.debug("Performing analysis...")
         comps_sizes_set = self.load_max_comps_sizes()
         try:
             for size in sizes:
                 assert size > 0
-                if size not in comps_sizes_set:
-                    comps_sizes_set[size] = {}
-                comps_sizes_subset = comps_sizes_set[size]
+                size_str = str(size)
+                if size_str not in comps_sizes_set:
+                    comps_sizes_set[size_str] = {}
+                comps_sizes_subset = comps_sizes_set[size_str]
 
                 for radius in radiuses:
                     assert radius >= 0.0 and radius <= 1.0
-                    if radius not in comps_sizes_subset:
-                        comps_sizes_subset[radius] = []
-                    comps_sizes = comps_sizes_subset[radius]
+                    radius_str = str(radius)
+                    if radius_str not in comps_sizes_subset:
+                        comps_sizes_subset[radius_str] = []
+                    comps_sizes = comps_sizes_subset[radius_str]
 
                     if len(comps_sizes) > count:
+                        # We arleady have this results
                         continue
 
-                    for index in range(len(comps_sizes), count):
-                        graph = self.generator(size, radius, index)
-                        comp_size = graph.max_component_size()
-                        comps_sizes.append(comp_size)
+                    indexes = range(len(comps_sizes), count)
+                    args = [(size, radius, index) for index in indexes]
+                    comps_sizes += pool.starmap(self.max_comp_size, args)
+
         finally:
             self.save_max_comps_sizes(comps_sizes_set)
 
         return comps_sizes_set
 
+    def max_comp_size(self, size: int, radius: float, index: int) -> int:
+        assert size > 0
+        assert radius >= 0.0 and radius <= 1.0
+        assert index >= 0
+
+        graph = self.generator(size, radius, index)
+        self.logger.debug("Calculating maximal component size for "
+                          " graph n=%s, r=%s (#%s)...", size, radius, index)
+        return graph.max_component_size()
+
     def load_max_comps_sizes(self) -> ComponentsSizesSet:
         path = Path(self.output_dir + "/max_comps_size.json")
         if not path.is_file():
-            self.log.warn("There is no maximal components sizes cache file")
+            self.logger.warn("There is no cached results file")
             return {}
 
-        self.log.debug("Loading cached maximal components sizes from file...")
+        self.logger.debug("Loading cached results from file...")
         with open(path, "r") as ifile:
             return json.load(ifile)
 
     def save_max_comps_sizes(self,
                              comps_sizes_set: ComponentsSizesSet) -> None:
-        self.log.debug("Saving maximal components size to file...")
+        self.logger.debug("Saving results to file...")
         path = Path(self.output_dir + "/max_comps_size.json")
         with open(path, "w") as ofile:
             return json.dump(comps_sizes_set, ofile)
